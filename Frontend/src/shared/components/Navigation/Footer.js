948eac000a067bf4776c6a0f3f4a360f5e0e1d74
sirish123 <cs20b043@iittp.ac.in>
2022-06-27 01:02:24 +0530

    footer done

--- Frontend/src/shared/components/Navigation/Footer.js
+++ Frontend/src/shared/components/Navigation/Footer.js
@@ -1,9 +1,25 @@
 import React from "react";
+import './Footer.css'
+const Footer = () => (
+  <div className="footer">
+    <div className="text-center">
+      <h3>Nature Wellness Center</h3>
+      <p>Contact No: Address:</p>
+      <a href="/#">
+      <i className="bi bi-linkedin"></i>
+      </a>
+      {"  "}
+      <a href="/#" >
+      <i className="bi bi-facebook"></i>
+      </a>
+      {"  "}
+      <a href="/#" >
+      <i className="bi bi-whatsapp text-success"></i>
+      </a>
 
-function Footer() {
-  return (
-    null
-  )
-}
+    </div>
+
+  </div>
+);
 
 export default Footer;
